1b9044437d24a66c6d2bfa717c694a5440998609
GloryKO <koladeg2022@gmail.com>
2023-06-23 11:43:26 -0700

    setup and added user,roles and applicants models

---
+++ applications/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+# Create your models here.
+from django import forms
+
+ROLES_CHOICES =(
+    
+        ("FRONTEND DEVELOPER","FRONTEND DEVELOPER"),
+        ("BACKEND DEVELOPER","BACKEND DEVELOPER"),
+        ("PRODUCT DESIGNER","PRODUCT DESIGNER"),
+        ("ML ENGINEER INTERN","ML ENGINEER INTERN"),
+        ("DIGITAL MARKETING INTERN","DIGITAL MARKETING INTERN"),
+        
+        )
+
+class Role(models.Model):
+    available_roles = models.CharField(max_length=25,choices=ROLES_CHOICES)
+    
+    def __str__(self):
+        return self.available_roles
+    
+    
+class Applicants(models.Model):
+    first_name= models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    resume_link = models.URLField()
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
